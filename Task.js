c07e2fe62e7275fc0893d9c374182277f8536bad
TurqaySafarov <161557826+TurqaySafarov@users.noreply.github.com>
2024-04-04 18:18:35 +0400

    first commit

---
+++ Task.js
@@ -0,0 +1,15 @@
+// Mehsur filmlerin movcud oldugu bir api url tapin. O apiden istifade ederek, filmin adlarini console-da gosterin
+
+
+// let film = prompt("film name: ");
+// fetch(`https://api.themoviedb.org/3/movie/top_rated?language=en-US&page={film}`)
+//   .then(response => response.json())
+//   .then(data => {
+//     data.results.map(movie => {
+//       console.log(movie.title);
+//     });
+//   })
+//   .catch(error => {
+//     console.error('xəta:', error);
+//   });
+
